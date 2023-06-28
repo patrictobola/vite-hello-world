9034ec6da3a31f0e842afdd2b856d5376328f8a7
patrictobola <tobola92@gmail.com>
2023-06-28 13:28:07 +0200

    AppTitle done

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <script>
-
+import AppTitle from './components/AppTitle.vue';
+export default {
+  components: {
+    AppTitle
+  }
+}
 </script>
-<template></template>
+
+
+<template>
+  <!-- Titolo -->
+  <AppTitle />
+</template>
+
+
 <style></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,5 @@
+<script></script>
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
+<style></style>
